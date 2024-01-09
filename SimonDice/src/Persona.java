284ccc9b14d5c41951bd22f16f2fad09373c44e5
MAQUINON <MAQUINON@DESKTOP-I4E7B84>
2024-01-09 12:55:10 +0100

    ponemos nuevas clases para completar el juego

---
+++ SimonDice/src/Persona.java
@@ -0,0 +1,10 @@
+
+public class Persona {
+	 private int puntuacion;
+	 private String name;
+	 
+	 public Persona(String _name ,int _puntuacion ) {
+		 this.name = _name;
+		 this.puntuacion = _puntuacion;
+	 }
+}
