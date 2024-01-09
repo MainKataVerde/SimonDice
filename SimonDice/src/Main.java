e0cb768cef587f11352b1a4afcfa9190407d4a6e
MAQUINON <MAQUINON@DESKTOP-I4E7B84>
2024-01-09 12:24:17 +0100

    segundo intento de sincronizar

--- SimonDice/src/Main.java
+++ SimonDice/src/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public enum tColores { Rojo , Verde , Azul , Dorado }
 
 	public static void main(String[] args) {
-		
 		play();
 		
 	}
@@ -26,19 +25,15 @@ public class Main {
         char letraColores = Character.toLowerCase(_color);
         
         	switch (letraColores) {
-			case 'R':
 			case 'r':
                 eleccionColores = tColores.Rojo;
 				break;
-			case 'V':
 			case 'v':
                 eleccionColores = tColores.Verde;
 				break;
-			case 'A':
 			case 'a':
                 eleccionColores = tColores.Azul;
 				break;
-			case 'D':
 			case 'd':
                 eleccionColores = tColores.Dorado;
 				break;
@@ -118,24 +113,17 @@ public class Main {
 	
 	public static void mostrarSecuencia (int _numero , tColores secuenciaColores[] ) {
 		System.out.print("Secuencia : ");
-		for (int i = 0; i < secuenciaColores.length; i++) {
+		for (int i = 0; i < _numero; i++) {
 			System.out.print(secuenciaColores[i] + " ");
 		}
 	}
 	
 	/**
-	 * Metodo para ejecutar
+	 * Metodo para ejecutar el juego 
 	 */
 	
 	public static void play() {
 		Scanner scanner = new Scanner(System.in);
-		
-		int MAX_COLORES_SEQ = 4 ;
-		
-        tColores secuenciaColores[] = new tColores[MAX_COLORES_SEQ];
-
-		generarSecuencia(4 , secuenciaColores);
-		
 		System.out.println("Welcome To Simon dice !");
         System.out.print("What is your name? ");
         String nombre = scanner.nextLine();
@@ -144,42 +132,56 @@ public class Main {
         if (scanner.hasNextLine()) {
             String s = scanner.nextLine();
         }
-
-		
-		mostrarSecuencia(MAX_COLORES_SEQ, secuenciaColores);
-		
-    	System.out.println();
-		
-		System.out.println("Pulsa ENTER cuando hayas memorizado todo");
-
-        if (scanner.hasNextLine()) {
-            String s = scanner.nextLine();
-        }
         
-        for (int i = 0; i < 50; ++i)
+        final int MAX_COLORES_SEQ = 12;
+		
+        tColores secuenciaColores[] = new tColores[MAX_COLORES_SEQ];
+        
+    	generarSecuencia(4 , secuenciaColores);
+        
+        
+        for (int i = 0; i < 12; i++) {
+        	
+        	
+
+        	
+    		
+    		mostrarSecuencia(3+i, secuenciaColores);
+    		
         	System.out.println();
-        
-		
-		System.out.print("Introduce los colores : ");
-		
-		char letra = scanner.next().charAt(0);
-								
-		int i = 0 ;
-		
-		if (comprobarColor(0, charColores(letra), secuenciaColores)==false) {
-			i++;
-			while (i<secuenciaColores.length) {
-				System.out.print("Introduce el siguiente color : ");
-				letra = scanner.next().charAt(0);
-				if (comprobarColor(i, charColores(letra), secuenciaColores)==false) {
-					i++;
-				}else {
-					i = secuenciaColores.length;
-					System.out.println("Fallaste");
-				}
-			}
-		}else {
-			System.out.println("Fallaste");
+    		
+    		System.out.println("Pulsa ENTER cuando hayas memorizado todo");
+
+            if (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+            }
+            
+           /* for (int k = 0; k < 50; k++)
+            	System.out.println();*/
+            
+    		System.out.print("Introduce los colores : ");
+    		
+    		char letra = scanner.next().charAt(0);
+    								
+    		int v = 0 ;
+    		
+    		if (comprobarColor(0, charColores(letra), secuenciaColores)==false) {
+    			v++;
+    			while (v<3+i) {
+    				System.out.print("Introduce el siguiente color : ");
+    				letra = scanner.next().charAt(0);
+    				if (comprobarColor(v, charColores(letra), secuenciaColores)==false) {
+    					v++;
+    				}else {
+    					v = secuenciaColores.length;
+    					System.out.println("Fallaste");
+    				}
+    			}
+    		}else {
+    			i = 13;
+    			System.out.println("Fallaste");//intento
+    		}
 		}
+		
 	}
 }
