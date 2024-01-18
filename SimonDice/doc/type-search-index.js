44148b050353f4a553795899ac7b170cf7f78cab
Oscar Nzabarinda Mukeshimana <101839576+MainKataVerde@users.noreply.github.com>
2024-01-18 18:14:49 +0100

    añadimos el javadoc bueno

--- SimonDice/doc/type-search-index.js
+++ SimonDice/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"engine"},{"p":"<Unnamed>","l":"engine.tColores"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"engine"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Persona"},{"p":"<Unnamed>","l":"engine.tColores"}];updateSearchResults();
